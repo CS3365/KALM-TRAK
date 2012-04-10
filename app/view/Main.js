47fb0d8cab27cc65de46117f3f7e0cbf71ff35c4
Mike Kent <zdz.ruai@gmail.com>
2012-04-10 10:26:57 -0500

    testing trigger

--- app/view/Main.js
+++ app/view/Main.js
@@ -20,7 +20,7 @@ Ext.define("KALMTRAK.view.Main", {
                 },
                 
                 html: [
-                    "test -You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "test2 -You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
                     "and refresh to change what's rendered here."
                 ].join("")
