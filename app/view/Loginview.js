7eff10cacfc82aa1ff9360f4904fedc04c663451
Louis Alridge <loualrid@gmail.com>
2012-04-24 18:27:08 -0500

    Didnt include new file..

---
+++ app/view/Loginview.js
@@ -0,0 +1,27 @@
+Ext.define("KALMTRAK.view.Loginview", {
+  extend: "Ext.form.Panel",
+  xtype: 'ktlogin',
+  requires: [
+    'Ext.form.Panel'
+  ],
+  config: {
+    title: 'KALMTRAK Login',
+    items: [
+		{
+		 xtype: 'fieldset',
+            title: 'KALMTRAK Login',
+            items: [
+              {
+                xtype: 'textfield',
+                name : 'name',
+                label: 'Username:'
+              },
+              {
+                xtype: 'passwordfield',
+                name : 'password',
+                label: 'Password:'
+              }
+            ]
+		}
+	]}
+});
