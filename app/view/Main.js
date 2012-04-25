b6cbaa6e43a2b5870ad538d326680c45c9c16b5b
Louis Alridge <loualrid@gmail.com>
2012-04-25 07:50:59 -0500

    testing button additions

--- app/view/Main.js
+++ app/view/Main.js
@@ -28,7 +28,36 @@ Ext.define("KALMTRAK.view.Main", {
                 xtype: 'passwordfield',
                 name : 'password',
                 label: 'Password:'
-              }
+              },
+			  {    xtype: 'toolbar',
+					layout: { pack: 'center' },
+					items: [
+						{
+							xtype: 'button',
+							text: 'Set Data',
+							handler: function() {
+								formPanel.setValues({
+									name: 'Ed',
+									password: 'secret'
+								})
+							}
+						},
+						{
+							xtype: 'button',
+							text: 'Submit',
+							handler: function() {
+								Ext.Msg.alert('Form Values', JSON.stringify(formPanel.getValues(), null, 2));
+							}
+						},
+						{
+							xtype: 'button',
+							text: 'Remember Me',
+							handler: function() {
+								formPanel.reset();
+							}
+						}
+					]
+				}
             ]
           }
         ]
@@ -41,36 +70,4 @@ Ext.define("KALMTRAK.view.Main", {
       }
     ]
   }
-});
-
-formPanel.add({
-    xtype: 'toolbar',
-    docked: 'bottom',
-    layout: { pack: 'center' },
-    items: [
-        {
-            xtype: 'button',
-            text: 'Set Data',
-            handler: function() {
-                formPanel.setValues({
-                    name: 'Ed',
-                    password: 'secret'
-                })
-            }
-        },
-        {
-            xtype: 'button',
-            text: 'Submit',
-            handler: function() {
-                Ext.Msg.alert('Form Values', JSON.stringify(formPanel.getValues(), null, 2));
-            }
-        },
-        {
-            xtype: 'button',
-            text: 'Remember Me',
-            handler: function() {
-                formPanel.reset();
-            }
-        }
-    ]
 });
