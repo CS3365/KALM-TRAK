a1c5202b660efe555f0b1ba8fe0813d9acb070dc
Louis Alridge <loualrid@gmail.com>
2012-04-25 07:47:09 -0500

    Testing form buttons

--- app/view/Main.js
+++ app/view/Main.js
@@ -30,14 +30,7 @@ Ext.define("KALMTRAK.view.Main", {
                 label: 'Password:'
               }
             ]
-          },
-		  {
-			xtype: 'button',
-            text: 'Login'
-            /*handler: function() {
-                Ext.Msg.alert('Form Values', JSON.stringify(formPanel.getValues(), null, 2));
-            }*/
-		  }
+          }
         ]
       },
       {
@@ -48,4 +41,36 @@ Ext.define("KALMTRAK.view.Main", {
       }
     ]
   }
+});
+
+formPanel.add({
+    xtype: 'toolbar',
+    docked: 'bottom',
+    layout: { pack: 'center' },
+    items: [
+        {
+            xtype: 'button',
+            text: 'Set Data',
+            handler: function() {
+                formPanel.setValues({
+                    name: 'Ed',
+                    password: 'secret'
+                })
+            }
+        },
+        {
+            xtype: 'button',
+            text: 'Submit',
+            handler: function() {
+                Ext.Msg.alert('Form Values', JSON.stringify(formPanel.getValues(), null, 2));
+            }
+        },
+        {
+            xtype: 'button',
+            text: 'Remember Me',
+            handler: function() {
+                formPanel.reset();
+            }
+        }
+    ]
 });
