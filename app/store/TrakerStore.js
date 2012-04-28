29ed6aa3050ed0063ad4e0d41baf32751360c6bc
Louis Alridge <loualrid@gmail.com>
2012-04-28 04:40:13 -0500

    Fixing up trakker list
    Signed-off-by: Louis Alridge <loualrid@gmail.com>

--- app/store/TrakerStore.js
+++ app/store/TrakerStore.js
@@ -13,21 +13,68 @@ Ext.define('KALMTRAK.store.TrakerStore', {
           text: 'Brilliant Trakers',
           items: [
             {
-              text: 'Water',
+              text: 'Kitchen',
               items: [
-                { text: 'Still', leaf: true },
-                { text: 'Sparkling', leaf: true }
+                { text: 'Stove', leaf: true }
               ]
             },
-            { text: 'Soda', leaf: true }
+            { text: 'KALMTRAK Lighting System', leaf:true },
+			{ text: 'Air Conditioning', leaf:true } 
           ]
         },
         {
           text: 'Normal Trakers',
           items: [
-            { text: 'Nuts', leaf: true },
-            { text: 'Pretzels', leaf: true },
-            { text: 'Wasabi Peas', leaf: true  }
+            { text: 'Foyer',
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true },
+				{ text: 'Lamp 3', leaf:true }
+				]
+			},
+            { text: 'Master Bedroom',
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true },
+				{ text: 'TV 1', leaf:true }
+				]
+			},
+            { text: 'Living Room',
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true },
+				{ text: 'TV 1', leaf:true }
+				]
+			},
+			{ text: 'Bedroom 1' ,
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true },
+				{ text: 'TV 1', leaf:true }
+				]
+			},
+			{ text: 'Upstairs Hallway',
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true }
+				]
+			},
+			{ text: 'Games Room',
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true },
+				{ text: 'TV 1', leaf:true },
+				{ text: 'XBox 360', leaf:true },
+				{ text: 'Wii', leaf:true }
+				]
+			},
+			{ text: 'Bedroom 2',
+				items:[
+				{ text: 'Lamp 1', leaf:true },
+				{ text: 'Lamp 2', leaf:true },
+				{ text: 'TV 1', leaf:true }
+				]
+			}
           ]
         }
       ]
