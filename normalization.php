7b6723379caa31a36e0bbfeb9a2ac2ade966a48d
Austin Ray <austin.william.ray@gmail.com>
2012-05-04 16:52:47 -0500

    normalization script 1st attempt

--- normalization.php
+++ normalization.php
@@ -36,13 +36,12 @@ $num_rows = mysql_num_rows($result);
 echo $num_rows . '<br>';
     
 for ($j = 0; $j < $num_rows; $j++){
-    echo 'in for loop <br>';
   // Get the tracker ID you want to update here. Hardcoded for the prototype
   $tracker_row = mysql_fetch_row($result);
   $tracker_id = $tracker_row[0]; 
   echo 'Traker ID: ' . $tracker_id . '<br>';
   // Get the last entry of a traker
-  $query = "SELECT * FROM powerticks WHERE tid=" . tracker_id . " ORDER BY `when` DESC LIMIT 1";
+  $query = "SELECT * FROM powerticks WHERE tid=" . $tracker_id . " ORDER BY `when` DESC LIMIT 1";
   $result = mysql_query($query);
 
   // Print any errors from retrieving result
